16ff37e0b053d9380de8d08167531c4c182b8a7d
Yaroslav <yr.pryhoda@gmail.com>
2023-05-15 18:38:09 +0300

    Added: Global Cache Interceptor for GET requests

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,26 @@
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { MulterModule } from '@nestjs/platform-express';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+
 import { DatabaseModule } from './modules/database/database.module';
 import { ApiModule } from './api/api.module';
-import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     MulterModule.register(),
+    CacheModule.register({ isGlobal: true, ttl: 30, max: 50 }),
     ApiModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
