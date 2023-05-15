3f54f4afb8203e167f2bf245715d5607fb02bf0a
Yaroslav <yr.pryhoda@gmail.com>
2023-05-15 19:15:54 +0300

    Added: In memory autocache

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,28 @@
 import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MulterModule } from '@nestjs/platform-express';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from './modules/database/database.module';
 import { ApiModule } from './api/api.module';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     MulterModule.register(),
+    BullModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        redis: {
+          maxRetriesPerRequest: 3,
+          host: configService.get('REDIS_URL'),
+          port: configService.get('REDIS_PORT'),
+        },
+      }),
+    }),
     CacheModule.register({ isGlobal: true, ttl: 30, max: 50 }),
     ApiModule,
   ],
